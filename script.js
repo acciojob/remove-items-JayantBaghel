6ecf7e21381998a30bfbace12588731ba9685a26
JayantBaghel <94191857+JayantBaghel@users.noreply.github.com>
2024-05-29 11:04:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.getElementById('removeButton').addEventListener('click', function() {
     const colorSelect = document.getElementById('colorSelect');
     const selectedOptionIndex = colorSelect.selectedIndex;
-    
+
+    console.log('Selected index:', selectedOptionIndex);
+    console.log('Options before removal:', Array.from(colorSelect.options).map(opt => opt.value));
+
     if (selectedOptionIndex !== -1) {
         colorSelect.remove(selectedOptionIndex);
     }
+
+    console.log('Options after removal:', Array.from(colorSelect.options).map(opt => opt.value));
 });
