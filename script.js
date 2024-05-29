3c5c4b970c34bdb5dc648b3b7accb6fcb35b21b9
JayantBaghel <94191857+JayantBaghel@users.noreply.github.com>
2024-05-29 10:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 document.getElementById('removeButton').addEventListener('click', function() {
     const colorSelect = document.getElementById('colorSelect');
     const selectedOptionIndex = colorSelect.selectedIndex;
